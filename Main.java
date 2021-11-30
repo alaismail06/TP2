41a4c412f38d0f41b259794d8d9dd9dcbe1d9dff
Afef-slh <80926132+Afef-slh@users.noreply.github.com>
2021-11-30 22:45:38 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package tn.isima.tp2;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -16,7 +16,7 @@ public class Main {
         job.setMapperClass(TokenizerMapper.class);
         job.setReducerClass(SumPriceReducer.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(DoubleWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
