162d6b3c8710668bd300aa876955eff8815d84e8
Afef-slh <80926132+Afef-slh@users.noreply.github.com>
2021-11-30 21:43:36 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package tn.isima.tp2;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.w3c.dom.Text;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "price count");
+        job.setJarByClass(Main.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setReducerClass(SumPriceReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
